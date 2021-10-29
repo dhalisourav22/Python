adf2739d42e1a292ceb60b6aba2a80cf487fd124
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-10-29 20:33:04 +0600

    Add files via upload

---
+++ Area Calculate/AreaOfCircle.py
@@ -0,0 +1,4 @@
+radiusOfCircle = float(input("Enter The Radius Of Circle : "))
+
+area = 3.1416*radiusOfCircle**2
+print("Area Of Circle Is : ",area)
---
+++ Area Calculate/AreaOfTraingle.py
@@ -0,0 +1,5 @@
+base = float(input("Enter The Base Of The Triangle : "))
+height = float(input("Enter The Height Of The Triangle : "))
+
+area = 0.5*base*height
+print("The Area Of Triangle Is : ",area)
