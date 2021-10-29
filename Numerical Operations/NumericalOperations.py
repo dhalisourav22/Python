69bb870f24c7a9cbe2fcd5ebd864a12ccdfc27cc
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-10-29 12:22:24 +0600

    Add files via upload

---
+++ Numerical Operations/NumericalOperations.py
@@ -0,0 +1,9 @@
+a=18
+b=4
+
+print("Sum Is : "+str(a+b))
+print("Sub Is : "+str(a-b))
+print('Multi and Division Is : '+str(a*b)+" and "+str(a/b) )
+print("Floor Valu Is : ")
+print(a//b)
+print("Exponential Value Is :",4**2)
