792de2a2c2c01e800f960252285ce7469ae10a42
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-10-29 09:55:33 +0600

    Update ShowOutput.py

--- Showing Output/ShowOutput.py
+++ Showing Output/ShowOutput.py
@@ -2,3 +2,7 @@ print(10)
 print(10.85)
 print("a")
 print("Sourav Dhali")
+
+#We will always use line 8 instead of line 7.(Demo of String Concatination.)
+print("Sourav"" Dhali")
+print("Sourav"+" Dhali")
