501e1525e8a36511d67e6ec64f5b2e8aa197d18d
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-10-29 12:05:41 +0600

    Add files via upload

---
+++ Variable/Variable.py
@@ -0,0 +1,13 @@
+name = "Sourav Dhali"
+uniName = 'NUBT'
+age = 23
+cgpa = 3.93
+
+print(name + uniName)
+print(age + cgpa)
+print(cgpa)
+
+print("Name Is : "+name)
+print("Name Is :",name)
+print("Age Is :",age)
+print("Age Is : "+str(age))
