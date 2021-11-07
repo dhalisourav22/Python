d0cd9637f9df24b9399680c07d5ca0f7c0fa118c
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-11-07 21:04:42 +0600

    Update AreaOfTraingle.py

--- Area Calculate/AreaOfTraingle.py
+++ Area Calculate/AreaOfTraingle.py
@@ -1,5 +1,6 @@
 base = float(input("Enter The Base Of The Triangle : "))
 height = float(input("Enter The Height Of The Triangle : "))
 
+
 area = 0.5*base*height
 print("The Area Of Triangle Is : ",area)
