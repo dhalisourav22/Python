bec802ec5d891ee13e10f198a953f792e1b95248
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-12-17 14:28:26 +0600

    Add files via upload

---
+++ Math Library Function/MathFun.py
@@ -0,0 +1,14 @@
+from math import sqrt
+result1 = max(10,30,20,15,31,35,10,22)
+result2 = min(10,30,20,15,31,35,10,22)
+print(type(result1))
+print("The Maximum Number is : ",result1)
+print("The Minimum Number is : ",result2)
+print(abs(-29))
+print(round(3.5))
+print(pow(2,4))   #means 2 to the power 4, that's means 16.
+print(sqrt(25))   #This is return Float type.
+
+from math import *
+print(floor(3.4))
+print(ceil(3.4))
