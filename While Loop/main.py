3cb2bea5ced7716369d74a31ccb324cb0d69fb5c
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-12-22 21:51:50 +0600

    Add files via upload

---
+++ While Loop/main.py
@@ -0,0 +1,20 @@
+#While Loop
+i = 1             #initialization
+while i<=100:     #condition. NB: The colne must.
+    print(i,end=", ")         #what is doing the loop.
+    i+=1                      #update or increment value of i.
+print("\nThis is out of the scope.")
+
+#Printing Even number.
+i = 2
+print("\nThe Even number's are : ")
+while(i<=100):     #we can use bracket to give condition like this.
+    print(i,end=", ")
+    i+=2
+
+#Printing Odd number.
+i = 1
+print("\n\nThe odd number's are : ")
+while(i<=100):     #we can use bracket to give condition like this.
+    print(i,end=", ")
+    i+=2
