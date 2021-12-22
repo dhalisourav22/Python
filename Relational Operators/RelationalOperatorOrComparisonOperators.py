3215c6a4715c09e90f024a3aa79e048cb5f58ba4
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-12-22 11:35:40 +0600

    Add files via upload

---
+++ Relational Operators/RelationalOperatorOrComparisonOperators.py
@@ -0,0 +1,15 @@
+#Here We Use Some Relational Operator.
+#This Type's Of Operator's Return Either True or False.(In Boolean Data Type)
+
+print(20<30)
+print(20>30)
+print(20<=30)
+print(20>=30)
+print(20==30)
+print(20!=30,end="\n\n")    #How This Print Function End With.
+
+
+#Compear String By "==" Operator. Which is Case sensetive.
+
+print("Sourav Dhali" == "Sourav Dhali")
+print("ABC"=="abc")
