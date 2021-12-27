2ce0b28e84fb9cddd5f63a2420d6a978a5992f1a
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-12-27 20:34:33 +0600

    Add files via upload

---
+++ Measurement of string/main.py
@@ -0,0 +1,16 @@
+userInput = input("Enter The String : ")
+noOfWords = 0
+noOfLetter = 0
+noOfNumber = 0
+
+for x in userInput:        #my name is 123
+    x.lower()
+    if x>="a" and x<="z":
+        noOfLetter+=1
+    elif x>="0" and x<="9":  #Here we use quotes because the 0 to 9 also string in userInput.
+        noOfNumber+=1
+    elif x == " ":
+        noOfWords+=1
+print("The number of letter :",noOfLetter)
+print("The number of Number :",noOfNumber)
+print("The number of Word :",noOfWords+1)
