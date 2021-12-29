a9ae903c9220c1acd3cbbf202cca59aaf56c42c6
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-12-29 12:56:54 +0600

    Update main.py

--- Add two matrix/main.py
+++ Add two matrix/main.py
@@ -2,11 +2,11 @@
 x=[[1,2,3],[4,5,6],[7,8,9]]            #no1 matrix
 y=[[11,12,13],[14,15,16],[17,18,19]]   #no2 matrix
 
-z=[[0,0,0],[0,0,0],[0,0,0]]        #Store Here the answer.
+z=[[0,0,0],[0,0,0],[0,0,0]]            #Store Here the answer.
 
 for rowIndex in range(len(x)):
     for colIndex in range(len(x[0])):
-        z[rowIndex][colIndex] = x[rowIndex][colIndex]+y[rowIndex][colIndex]
+        z[rowIndex][colIndex] = x[rowIndex][colIndex]+y[rowIndex][colIndex]           #resultMatrix = no1Matrix + no2matrix
 
 print("The Matrix Is : ")
 for row in z:
