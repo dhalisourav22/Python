71e455854120ab50943502a550dafe73bd3a9f50
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-12-30 15:17:12 +0600

    Update main.py

--- Measurement of string/main.py
+++ Measurement of string/main.py
@@ -3,7 +3,7 @@ noOfWords = 0
 noOfLetter = 0
 noOfNumber = 0
 
-for x in userInput:        #This is check each and every
+for x in userInput:        #This is check each and every character
     x.lower()
     if x>="a" and x<="z":
         noOfLetter+=1
