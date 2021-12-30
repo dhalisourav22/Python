e43ac7ae7fa7acc3255d6fd39e94ead1029ca915
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-12-30 15:19:02 +0600

    Update main.py

--- Measurement of string/main.py
+++ Measurement of string/main.py
@@ -3,11 +3,11 @@ noOfWords = 0                       #For counting.
 noOfLetter = 0
 noOfNumber = 0
 
-for x in userInput:        #This is check each and every character of string.
+for x in userInput:                 #This is check each and every character of string.
     x.lower()
     if x>="a" and x<="z":
         noOfLetter+=1
-    elif x>="0" and x<="9":  #Here we use quotes because the 0 to 9 also string in userInput.
+    elif x>="0" and x<="9":         #Here we use quotes because the 0 to 9 also string in userInput.
         noOfNumber+=1
     elif x == " ":
         noOfWords+=1
