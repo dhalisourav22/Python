518f337cb6ffcca4e526f0c89f15a932b797bb91
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2022-01-01 09:55:34 +0600

    Update NumericalOperations.py

--- Numerical Operations/NumericalOperations.py
+++ Numerical Operations/NumericalOperations.py
@@ -1,7 +1,7 @@
 a=18
 b=4
 
-print("Sum Is : "+str(a+b))     #str() convert any data to any type to string type.
+print("Sum Is : "+str(a+b))     #str() convert any data to any type to string type. Here we need to convert an integer type data to a string type data, for print by '+' sign under print function.
 print("Sub Is : "+str(a-b))
 print('Multi and Division Is : '+str(a*b)+" and "+str(a/b) )
 print("Floor Valu Is : ")
