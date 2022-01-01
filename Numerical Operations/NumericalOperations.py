d351f3ea741595edeebc46d7d654f73a71a734c4
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2022-01-01 09:54:33 +0600

    Update NumericalOperations.py

--- Numerical Operations/NumericalOperations.py
+++ Numerical Operations/NumericalOperations.py
@@ -1,7 +1,7 @@
 a=18
 b=4
 
-print("Sum Is : "+str(a+b))
+print("Sum Is : "+str(a+b))     #str() convert any data to any type to string type.
 print("Sub Is : "+str(a-b))
 print('Multi and Division Is : '+str(a*b)+" and "+str(a/b) )
 print("Floor Valu Is : ")
