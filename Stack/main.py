017f64d29b23decc7fee7e835ce184c4d5f257f5
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2022-01-06 17:24:41 +0600

    Add files via upload

---
+++ Stack/main.py
@@ -0,0 +1,21 @@
+#creating a stack by using list. We can use all list operation here. because this is a list.
+thisStack = ["Sourav","Juthi","Sumi","Shuvo","Mehedi"]
+
+#push an item.
+thisStack.append("Kamal")
+print("After adding an item into the stack :",thisStack)
+
+#pop an item.
+thisStack.pop()
+print("After pop :",thisStack)
+print("The top item is :",thisStack[-1])
+print("We have :",len(thisStack),"item's.")
+thisStack.pop()
+thisStack.pop()
+thisStack.pop()
+thisStack.pop()
+thisStack.pop()
+print("After pop 5 time :",thisStack)
+
+if not thisStack:                   #if the collection is empty then it will return True.
+    print("The stack is empty.!")
