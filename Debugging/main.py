c5fa71f057abc8c48e8d2bda7a1ff5c1e5c2ab70
Sourav Dhali <dhalisourav22@gmail.com>
2022-01-25 11:28:31 +0600

    Update main.py

--- Debugging/main.py
+++ Debugging/main.py
@@ -1,3 +1,4 @@
+#We habe basically 3 step. 1. Select line from where we want to start debugging.  2. Start debugging by (Run >> Debug'fileName') / (shift+F9)  3. Change line by one by one by which we debug the segment of code step by spep. for this we have to click (step into) / F7.
 def add(*numbers):
     sum=0
     for num in numbers:
